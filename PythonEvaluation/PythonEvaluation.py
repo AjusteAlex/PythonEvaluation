fa5e3082052ca0c97c35ca56d66fb72ac5dcd81f
AjusteAlex <alexandre.ajuste@gmail.com>
2020-12-09 09:58:40 +0100

    update

--- PythonEvaluation/PythonEvaluation.py
+++ PythonEvaluation/PythonEvaluation.py
@@ -4,10 +4,9 @@ taille = int(input("Entrez une taille :"))
 colonne = 0
 ligne = 0
 
-for ligne in range(0, taille):
-    for colonne in range(0, taille, 2):
-        print(caractere, end="")
-        print(" ", end="")
+for colonne in range(0, taille):
+    for ligne in range(0, taille, 2):
+        print(caractere, end=" ")
     print("")
-    if (ligne%2 == 0): 
+    if (colonne%2 == 0): 
         print(" ", end="")
