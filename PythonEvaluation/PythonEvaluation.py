9950b6733e1ad5ae9054b5e6476d1d2016cf401a
AjusteAlex <alexandre.ajuste@gmail.com>
2020-12-09 09:44:43 +0100

    evaluation python

--- PythonEvaluation/PythonEvaluation.py
+++ PythonEvaluation/PythonEvaluation.py
@@ -1,7 +1,13 @@
-print("A A A ")
-print(" A A A")
-print("A A A ")
-print(" A A A")
-print("A A A ")
-print(" A A A")
+caractere = input("Entrez un caractère :")
+taille = int(input("Entrez une taille :"))
 
+colonne = 0
+ligne = 0
+
+for ligne in range(0, taille):
+    for colonne in range(0, taille, 2):
+        print(caractere, end="")
+        print(" ", end="")
+    print("")
+    if (ligne%2 == 0): 
+        print(" ", end="")
