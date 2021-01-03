1e695d927b8935bd08664f5181d5e92c589456d6
AjusteAlex <alexandre.ajuste@gmail.com>
2021-01-03 21:20:35 +0100

    remove files

--- PythonEvaluation/PythonEvaluation.py
+++ PythonEvaluation/PythonEvaluation.py
@@ -1,15 +1,35 @@
-caractere = input("Entrez un caractère :")
-taille = int(input("Entrez une taille :"))
 
-colonne = 0
-ligne = 0
+# DAMIER #
 
-for colonne in range(0, taille):
-    for ligne in range(0, 4):
-        print(caractere, end=" ")
-    print("")
-    if (colonne%2 == 0): 
-        print(" ", end="")
+# caractere = input("Entrez un caractère :")
+# taille = int(input("Entrez une taille :"))
+
+# colonne = 0
+# ligne = 0
+
+# for colonne in range(0, taille):
+#     for ligne in range(0, 4):
+#         print(caractere, end=" ")
+#     print("")
+#     if (colonne%2 == 0): 
+#         print(" ", end="")
+
+
+
+#####################################################
+
+# SPIRALE #
+
+caractere = input("Entrez un caractère : ")
+longueur = input("Entrez une longueur : ")
+
+
+for largeur in range(0, longueur)
+    for hauteur in range(0, longueur-1)
+
+
+
+
 
 
 
