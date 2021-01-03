bc86655da7f58557a24e5a7d5237d103c28ff4ed
AjusteAlex <alexandre.ajuste@gmail.com>
2021-01-03 17:58:43 +0100

    amélioration code

--- PythonEvaluation/PythonEvaluation.py
+++ PythonEvaluation/PythonEvaluation.py
@@ -5,8 +5,11 @@ colonne = 0
 ligne = 0
 
 for colonne in range(0, taille):
-    for ligne in range(0, taille, 2):
+    for ligne in range(0, 4):
         print(caractere, end=" ")
     print("")
     if (colonne%2 == 0): 
         print(" ", end="")
+
+
+
